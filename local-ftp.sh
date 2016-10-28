32b9054969f1b8dea97141fe0309a5635f2b9ea5
Joseph Dykstra <josephdykstra@gmail.com>
2016-10-27 22:18:56 -0500

    Use local ftp folder

---
+++ local-ftp.sh
@@ -0,0 +1,8 @@
+apt-get install -y vsftpd
+
+echo "listen=YES" > /etc/vsftpd.conf
+echo "dirmessage_enable=YES" >> /etc/vsftpd.conf
+echo "xferlog_enable=YES" >> /etc/vsftpd.conf
+echo "secure_chroot_dir=/var/run/vsftpd/empty" >> /etc/vsftpd.conf
+
+service vsftpd restart
